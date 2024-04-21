fd319b20cb2f6e0096b8306068a55ac80f8f151c
Jan Erik Hanssen <jhanssen@gmail.com>
2024-04-20 22:32:58 -0700

    fix shader generation deps

--- src/render/shaders/shaders.cmake
+++ src/render/shaders/shaders.cmake
@@ -1,12 +1,14 @@
 set(GLSLC ${THIRDPARTY_BINARY_DIR}/shaderc/glslc/glslc)
+set(SHADER_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/shaders)
 set(SHADER_BINARY_DIR ${CMAKE_BINARY_DIR}/bin/shaders)
 file(MAKE_DIRECTORY ${SHADER_BINARY_DIR})
+add_dependencies(spurv-render glslc_exe)
 
 macro(add_shader FILE STAGE)
     add_custom_command(
         OUTPUT ${SHADER_BINARY_DIR}/${FILE}.spv
-        COMMAND ${GLSLC} -fshader-stage=${STAGE} ${CMAKE_CURRENT_SOURCE_DIR}/shaders/${FILE}.glsl -o ${SHADER_BINARY_DIR}/${FILE}.spv
-        DEPENDS shaderc)
+        COMMAND ${GLSLC} -fshader-stage=${STAGE} ${SHADER_SOURCE_DIR}/${FILE}.glsl -o ${SHADER_BINARY_DIR}/${FILE}.spv
+        DEPENDS glslc_exe ${SHADER_SOURCE_DIR}/${FILE}.glsl)
     add_custom_target(${FILE} DEPENDS ${SHADER_BINARY_DIR}/${FILE}.spv)
     add_dependencies(spurv-render ${FILE})
 endmacro()
