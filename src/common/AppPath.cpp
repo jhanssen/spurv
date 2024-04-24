505de6d102b1481d4192d8f814a487dcf76d98c9
Jan Erik Hanssen <jhanssen@gmail.com>
2024-04-23 18:47:59 -0700

    betterer

--- src/common/AppPath.cpp
+++ src/common/AppPath.cpp
@@ -10,13 +10,12 @@ namespace spurv {
 std::filesystem::path appPath()
 {
 #if defined(__linux__)
-    // ### should error check, follow links recursively
-    return std::filesystem::read_symlink("/proc/self/exe").parent_path();
+    return std::filesystem::canonical(std::filesystem::path("/proc/self/exe")).parent_path();
 #elif defined(__APPLE__)
     char buf[PATH_MAX];
     uint32_t size = sizeof(buf);
     if (_NSGetExecutablePath(buf, &size) == 0) {
-        return std::filesystem::path(std::string(buf, size)).parent_path();
+        return std::filesystem::canonical(std::filesystem::path(std::string(buf, size))).parent_path();
     }
     return {};
 #else
