81777c8641424ee22423dbeb0335f1c80c508a7b
Jan Erik Hanssen <jhanssen@gmail.com>
2024-04-25 02:04:23 -0700

    seems like we need to free the exception atoms explicitly

--- src/script/JSExtra.cpp
+++ src/script/JSExtra.cpp
@@ -1,12 +1,26 @@
 #include "JSExtra.h"
+#include "ScriptEngine.h"
 #include <cstddef>
+
+void JS_FreeException(JSContext* ctx, JSValueConst obj)
+{
+    if (!JS_IsObject(obj) && !JS_IsNull(obj)) {
+        return;
+    }
+    const auto& atoms = spurv::ScriptEngine::scriptEngine()->atoms();
+    auto message = JS_GetProperty(ctx, obj, atoms.message);
+    if (!JS_IsUndefined(message)) {
+        JS_FreeValue(ctx, message);
+    }
+    JS_FreeValue(ctx, obj);
+}
 
 // ### this includes SharedArrayBuffer
 bool JS_IsArrayBuffer(JSContext *ctx, JSValueConst obj)
 {
     std::size_t p;
     const bool isab = JS_GetArrayBuffer(ctx, &p, obj);
-    JS_GetException(ctx);
+    JS_FreeException(ctx, JS_GetException(ctx));
     return isab;
 }
 
@@ -15,7 +29,7 @@ bool JS_IsTypedArray(JSContext *ctx, JSValueConst obj)
 {
     JSValue val = JS_GetTypedArrayBuffer(ctx, obj, nullptr, nullptr, nullptr);
     if (JS_IsException(val)) {
-        JS_GetException(ctx);
+        JS_FreeException(ctx, JS_GetException(ctx));
         return false;
     }
 
