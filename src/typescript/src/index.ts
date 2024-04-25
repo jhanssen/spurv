d4c8a7f3aee83031ae33d337f129f5c18e4a1292
Anders Bakken <agbakken@gmail.com>
2024-04-24 18:22:38 -0700

    Quit on q.

--- src/typescript/src/index.ts
+++ src/typescript/src/index.ts
@@ -4,13 +4,16 @@ import { error } from "./log";
 // Object.defineProperty(globalThis, "Process", Process);
 
 spurv.setKeyEventHandler((event: spurv.KeyEvent) => {
-    error("got key event", JSON.stringify(event, undefined, 4), "fuck");
+    error("got key event", JSON.stringify(event, undefined, 4));
+    if (event.key === 81 && event.action === 1) {
+        exit(0);
+    }
 });
 
 error("testing");
 error("testing2");
 
-setTimeout(() => {
-    error("about to exit");
-    exit(10);
-}, 2000);
+// setTimeout(() => {
+//     error("about to exit");
+//     exit(10);
+// }, 2000);
