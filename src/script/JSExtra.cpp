87784e0ac04d4f1b56c0c5969014e277b60de9e4
Jan Erik Hanssen <jhanssen@gmail.com>
2024-04-25 01:14:48 -0700

    clear exceptions in IsTypedArray and IsArrayBuffer

--- src/script/JSExtra.cpp
+++ src/script/JSExtra.cpp
@@ -5,7 +5,9 @@
 bool JS_IsArrayBuffer(JSContext *ctx, JSValueConst obj)
 {
     std::size_t p;
-    return JS_GetArrayBuffer(ctx, &p, obj);
+    const bool isab = JS_GetArrayBuffer(ctx, &p, obj);
+    JS_GetException(ctx);
+    return isab;
 }
 
 // ### this includes DataView
@@ -13,6 +15,7 @@ bool JS_IsTypedArray(JSContext *ctx, JSValueConst obj)
 {
     JSValue val = JS_GetTypedArrayBuffer(ctx, obj, nullptr, nullptr, nullptr);
     if (JS_IsException(val)) {
+        JS_GetException(ctx);
         return false;
     }
 
